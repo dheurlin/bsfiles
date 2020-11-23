4d8cfba3eac2ff889eff98a600c01eeb9aaa44de
Daniel Heurlin <daniel.heurlin@gmail.com>
2020-11-23 23:42:52 +0100

    Basic file upload implemented

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 A simple file upload client with authentication. Written in Python and Flask.
+
+## TODO write about:
+
+* Secret variables
+* Creating a user
+* aliases.sh
+* migrations
--- project/bsfiles/main.py
+++ project/bsfiles/main.py
@@ -1,17 +1,34 @@
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+import os
+
+from flask import Blueprint, flash, redirect, render_template, request, url_for, current_app
 from flask_login import login_required, login_user, logout_user
 from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
 
 from . import db
 from .models import User
 
 main = Blueprint('main', __name__)
 
-
 @main.route('/')
 @login_required
 def index():
     return render_template('index.html')
+
+@main.route('/', methods=('POST',))
+@login_required
+def upload_file():
+    # check if the post request has the file part
+    file = request.files.get('file')
+    if file is None or file.filename == '':
+        flash('No file part')
+        return redirect(url_for('main.index'))
+
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+
+    flash('File uploaded successfully')
+    return redirect(url_for('main.index'))
 
 
 @main.route('/login')
