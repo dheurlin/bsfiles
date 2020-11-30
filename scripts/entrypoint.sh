ea705fc72287195da7991e1c1d376bfe2db5c93f
Daniel Heurlin <daniel.heurlin@gmail.com>
2020-11-30 10:27:35 +0100

    Make FLASK_ENV variable in secrets file

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,6 +7,4 @@ flask db upgrade
 # Watch SASS folder
 sass --watch /project/bsfiles/static/sass:/project/bsfiles/static/css &
 
-# Start the Flask development server
-export FLASK_ENV=development
 flask run --host=0.0.0.0
